98508d2ec7486f5485e2fe57c0cc2e813d8be0ed
avarshvir <avarshvir@gmail.com>
2026-02-08 23:10:59 +0530

    Sum of 1D Array

--- 1_two_sum/1_two_sum.cpp
+++ 1_two_sum/1_two_sum.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_to_index; 
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];  
-            
-            if (num_to_index.find(complement) != num_to_index.end()) {
-                return {num_to_index[complement], i};
-            }
-            
-            num_to_index[nums[i]] = i;
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] = nums[i] + nums[i - 1];
         }
-        
-        return {};
+        return nums;
     }
 };
