ea34ded7e0adf695e26b4bb24c8fa3ad96b10368
avarshvir <avarshvir@gmail.com>
2026-02-08 23:11:39 +0530

    Sum of 1D Array

--- 1480_Running_Sum_of_1D_Array/1480_running_sum_of_1d_array.cpp
+++ 1480_Running_Sum_of_1D_Array/1480_running_sum_of_1d_array.cpp
@@ -3,15 +3,15 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-         vector<int> result(nums.size());  // Create a result vector of the same size as nums
-        int currentSum = 0;  // Initialize the cumulative sum to 0
+         vector<int> result(nums.size());  
+        int currentSum = 0;  
 
         for (int i = 0; i < nums.size(); ++i) {
-            currentSum += nums[i];  // Add the current element to the cumulative sum
-            result[i] = currentSum;  // Store the cumulative sum in the result vector
+            currentSum += nums[i];  
+            result[i] = currentSum;  
         }
 
-        return result;  // Return the result vector containing the running sums
+        return result; 
     
     }
 };
