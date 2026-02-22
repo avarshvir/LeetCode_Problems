70cc790a55f5cfc9c8fc46fa3774c6400b01c1d5
avarshvir <avarshvir@gmail.com>
2026-02-22 23:05:03 +0530

    66 Plus One

--- 66_Plus_One/solution.cpp
+++ 66_Plus_One/solution.cpp
@@ -4,7 +4,7 @@ public:
         for(int i = digits.size() - 1; i >= 0; i--){
             if(digits[i] < 9){
                 digits[i]++;
-                return true;
+                return digits;
             }
             digits[i] = 0;
         }
