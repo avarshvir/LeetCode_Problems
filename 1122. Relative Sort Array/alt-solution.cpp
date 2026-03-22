81620f5378f682c0c30c7a5b87b55a6630a1eca9
avarshvir <avarshvir@gmail.com>
2026-03-22 22:32:40 +0530

    1380 question

--- 1122. Relative Sort Array/alt-solution.cpp
+++ 1122. Relative Sort Array/alt-solution.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int, int> pos;
 
-        // Store positions of elements in arr2
         for(int i = 0; i < arr2.size(); i++){
             pos[arr2[i]] = i;
         }
 
-        // Custom sort
         sort(arr1.begin(), arr1.end(), [&](int a, int b) {
             if(pos.count(a) && pos.count(b))
                 return pos[a] < pos[b];
