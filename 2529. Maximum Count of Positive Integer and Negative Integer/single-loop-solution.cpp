2a26226468c785624e004b292e87343281bceda0
avarshvir <avarshvir@gmail.com>
2026-03-23 20:15:10 +0530

    O(n)

---
+++ 2529. Maximum Count of Positive Integer and Negative Integer/single-loop-solution.cpp
@@ -0,0 +1,11 @@
+class Solution { 
+    public: 
+    int maximumCount(vector<int>& nums) { 
+        int neg = 0, pos = 0; 
+        for (int x : nums) { 
+            if (x < 0) neg++; 
+            else if (x > 0) pos++; 
+        } 
+        return max(neg, pos); 
+    } 
+};
