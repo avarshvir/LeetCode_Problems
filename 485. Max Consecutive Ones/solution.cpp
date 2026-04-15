818d8a095df3582a56c5149a473aaf9e38b11e55
avarshvir <avarshvir@gmail.com>
2026-04-15 21:12:38 +0530

    T: O(n) - S: O(1)

---
+++ 485. Max Consecutive Ones/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int current_count = 0;
+        int max_count = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 1){
+                current_count++;
+                max_count = max(max_count, current_count);
+            } else {
+                current_count = 0;
+            }
+        }
+
+        return max_count;
+    }
+};
