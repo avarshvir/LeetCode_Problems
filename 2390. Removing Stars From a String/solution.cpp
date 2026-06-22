e97a21ccd92d5555ce66929462dc39993a398c47
avarshvir <avarshvir@gmail.com>
2026-06-22 21:09:27 +0530

    2390 solution

---
+++ 2390. Removing Stars From a String/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeStars(string s){
+        stack<char> c;
+        for(auto i: c){
+            c.push(i);
+            if(i == '*'){
+                c.pop();
+                c.pop();
+            }
+        }
+        string s2;
+        while(!c.empty()){
+            s2 += c.top();
+            c.pop(); 
+        }
+        reverse(s2.begin(), s2.end());
+
+        return s2;
+    }
+}
