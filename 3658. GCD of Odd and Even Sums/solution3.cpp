f346ddce74fa09112f1853f5d141d67644d00a23
avarshvir <avarshvir@gmail.com>
2026-07-15 21:53:55 +0530

    solution3

---
+++ 3658. GCD of Odd and Even Sums/solution3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        
+        // Loop through every number from 1 up to n
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                // If the remainder is 0, the number is even
+                sumEven += i;
+            } else {
+                // Otherwise, the number is odd
+                sumOdd += i;
+            }
+        }
+        
+        // Return the Greatest Common Divisor of the two sums
+        return std::gcd(sumOdd, sumEven);
+    }
+};
