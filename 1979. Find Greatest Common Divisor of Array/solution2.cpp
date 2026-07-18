d9f76588f787f9674d01b931974a987f962be0a5
avarshvir <avarshvir@gmail.com>
2026-07-18 14:49:18 +0530

    1979 solution

---
+++ 1979. Find Greatest Common Divisor of Array/solution2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int minimum = nums[0];
+        int maximum = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < minimum){
+                minimum = nums[i];
+            }
+            if(nums[i] > maximum){
+                maximum = nums[i];
+            }
+        }
+
+        return gcd(minimum, maximum);
+
+    }
+};
